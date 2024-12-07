6865a70d84dbc058eb15d0a555804b1540998fc0
Fire Phoenix <3301647@qq.com>
2024-12-07 16:25:47 +0800

    add NonUniformMatrixXf;

--- Phoenix/public/shape/BoundedTensor3f.h
+++ Phoenix/public/shape/BoundedTensor3f.h
@@ -12,24 +12,12 @@ using RowTensor3f = Eigen::Tensor<float, 3, Eigen::RowMajor>;
 
 class BoundedTensor3f : public RowTensor3f {
 public:
-    // 默认构造函数
     BoundedTensor3f();
 
-    // 从张量和边界构造
     BoundedTensor3f(const RowTensor3f& tensor, const Bounds2f& bounds);
 
-    // 获取边界
-    const Bounds2f& getBounds() const {
-        return bounds;
-    }
-
-    // 设置边界
-    void setBounds(const Bounds2f& newBounds) {
-        bounds = newBounds;
-    }
-
-private:
-    Bounds2f bounds; // 2D边界信息
+public:
+    Bounds2f bounds; // 2D bounds
 };
 } // namespace Phoenix
 #endif // PHOENIX_BoundedTensor3f_H
